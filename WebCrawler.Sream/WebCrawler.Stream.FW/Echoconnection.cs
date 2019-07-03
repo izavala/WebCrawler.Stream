0e04c47c963af3f6262335f759c596dae1a75faf
Isaac Z <isaac.zavala@petabridge.com>
2019-07-03 11:10:03 -0500

    Display webcrawler results through signal R

--- WebCrawler.Sream/WebCrawler.Stream.FW/Echoconnection.cs
+++ WebCrawler.Sream/WebCrawler.Stream.FW/Echoconnection.cs
@@ -5,6 +5,7 @@ using Akka.Actor;
 using Akka.Streams;
 using Akka.Streams.Dsl;
 using Akka.Streams.SignalR;
+using Microsoft.Owin.Security.Provider;
 using WebCrawler.Stream.FW;
 
 
@@ -19,17 +20,20 @@ namespace WebCrawler.Stream
             var graph = RunnableGraph.FromGraph(GraphDsl.Create(b =>
             {
                 var source = Source.Collect(x => x as Received).Select(x => Signals.Broadcast(x.Data));
-                var f1 = Flow.Create<ISignalRResult>().Select(x => x.Data.ToString());
-                var f2 = Flow.Create<string>().Select(x => new Uri(x));
+                var f1 = Flow.Create<ISignalRResult>().Select(x => x.Data);
+                var f2 = Flow.Create<string>().Select(x => new Uri(x)).Recover(exception =>
+                {
+                    if(exception is System.UriFormatException)
+                        return new Uri(null);
+                    throw new Exception("error");
+                });
                 var f3 = Flow.Create<Uri>().Select(x => Signals.Broadcast(x.ToString()));
+                var f4 = Flow.Create<string>().Select(x => Signals.Broadcast(x));
                 var merge = b.Add(new Merge<ISignalRResult>(2));
-
-                var sink = b.Add(Akka.Streams.Dsl.Sink.ForEach<Uri>(Console.WriteLine));
                 var crawlerFlow = b.Add(Crawler.MyCrawler());
-                var broadcast = b.Add(new Broadcast<ISignalRResult>(2));
-                b.From(source).Via(broadcast).To(this.Sink);
-                b.From(broadcast).Via(f1).Via(f2).Via(crawlerFlow).Via(f3).To(this.Sink);
-
+                var broadcast = b.Add(new Broadcast<string>(2));
+                b.From(source).Via(f1).Via(broadcast).Via(f4).Via(merge).To(this.Sink);
+                b.From(broadcast).Via(f2).Via(crawlerFlow).Via(f3).Via(merge);
                 return ClosedShape.Instance;
             }));
             graph.Run(App.Materializer);
